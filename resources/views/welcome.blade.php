18ed40723c01f162abc2804759f184cc4309b2b2
Henrique <the.devs.drc@gmail.com>
2025-02-19 16:50:23 -0300

    adicionando o arquivo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TechID</title>
+    <style>
+        button{
+            border: 1px #000 solid,
+            border-radius: 15px,
+        }
+    </style>
+</head>
+<body>
+    <input name="display" type="text">
+    <button class="btn1">1</button>
+    <button class="btn2">2</button>
+    <button class="btn3">3</button>
+    <button class="btn4">4</button>
+    <button class="btn5">5</button>
+    <button class="btn6">6</button>
+    <button class="btn7">7</button>
+    <button class="btn8">8</button>
+    <button class="btn9">9</button>
+    <button class="btn0">0</button>
+</body>
+</html>
